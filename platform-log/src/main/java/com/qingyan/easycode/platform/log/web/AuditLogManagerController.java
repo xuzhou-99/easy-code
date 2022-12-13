9a6354144b396ce62e09843c6cd3fa436a3cdfb0
csriver <1064284972@qq.com>
2022-12-13 15:57:06 +0800

    :construction: 修改日志依赖版本 v1.0.1 && 优化代码

--- platform-log/src/main/java/com/qingyan/easycode/platform/log/web/AuditLogManagerController.java
+++ platform-log/src/main/java/com/qingyan/easycode/platform/log/web/AuditLogManagerController.java
@@ -1,27 +1,3 @@
-/*
- * Copyright [2020-2030] [https://www.stylefeng.cn]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Guns采用APACHE LICENSE 2.0开源协议，您在使用过程中，需要注意以下几点：
- *
- * 1.请不要删除和修改根目录下的LICENSE文件。
- * 2.请不要删除和修改Guns源码头部的版权声明。
- * 3.请保留源码和相关描述文件的项目出处，作者声明等。
- * 4.分发源码时候，请注明软件出处 https://gitee.com/stylefeng/guns
- * 5.在修改包名，模块名称，项目代码等时，请注明软件出处 https://gitee.com/stylefeng/guns
- * 6.若您的项目无法满足以上几点，可申请商业授权
- */
 package com.qingyan.easycode.platform.log.web;
 
 import java.util.List;
@@ -50,7 +26,7 @@ import cn.altaria.base.response.ApiResponse;
  *
  * @author xuzhou
  * @version v1.0.0
- * @since  2022/2/10 16:17
+ * @since 2022/2/10 16:17
  */
 @RestController
 @RequestMapping(name = "日志管理控制器")
@@ -64,6 +40,9 @@ public class AuditLogManagerController {
 
     /**
      * 查询日志列表
+     *
+     * @param logManagerRequest 日志查询参数
+     * @return 日志列表
      */
     @GetMapping(name = "查询日志列表", path = "/logManager/list")
     public ApiDataResponse<List<AuditLogRecordPojo>> list(@RequestBody LogManagerRequest logManagerRequest) {
@@ -72,7 +51,10 @@ public class AuditLogManagerController {
     }
 
     /**
-     * 查询日志
+     * 查询日志-分页
+     *
+     * @param logManagerRequest 日志查询参数
+     * @return 日志列表
      */
     @GetMapping(name = "查询日志列表", path = "/logManager/page")
     public ApiDataResponse<Page<AuditLogRecordPojo>> page(LogManagerRequest logManagerRequest) {
@@ -82,6 +64,9 @@ public class AuditLogManagerController {
 
     /**
      * 删除日志
+     *
+     * @param logManagerRequest 日志删除参数
+     * @return 操作结果
      */
     @PostMapping(name = "删除日志", path = "/logManager/delete")
     @AuditLog(description = "删除日志", module = AuditLogModuleEnum.DATA, operationType = AuditLogOperationTypeEnum.delete)
@@ -92,6 +77,9 @@ public class AuditLogManagerController {
 
     /**
      * 查看日志详情
+     *
+     * @param logManagerRequest 日志查询参数
+     * @return 日志详情
      */
     @GetMapping(name = "查看日志详情", path = "/logManager/detail")
     public ApiDataResponse<AuditLogRecordPojo> detail(LogManagerRequest logManagerRequest) {
